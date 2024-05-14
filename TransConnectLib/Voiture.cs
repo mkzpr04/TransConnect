db5477ae5f213594d86cd2fd3427feb34eb6be9b
Ga5pard <gaspard.tournadre@gmail.com>
2024-05-15 00:32:08 +0200

    Update Voiture.cs
    Ajout de méthodes

--- TransConnectLib/Voiture.cs
+++ TransConnectLib/Voiture.cs
@@ -7,33 +7,37 @@ using System.Threading.Tasks;
 namespace TransConnectLib
 {
     public class Voiture : Vehicule
+{
+    public int NombrePassagers { get; private set; } // Le nombre de passagers 
+
+    public Voiture(Chauffeur chauffeur, string plaqueImmatriculation, string marque, string modele,
+                   double capaciteReservoir, string couleur, int nombrePassagers)
+                   : base(chauffeur, plaqueImmatriculation, marque, modele, capaciteReservoir, couleur)
     {
-        public int NombrePassagers;
+        this.NombrePassagers = nombrePassagers;
+    }
 
-        public Voiture(Chauffeur chauffeur, string plaqueImmatriculation, string marque, string modele,
-                       double capaciteReservoir, string couleur, int nombrePassagers)
-                       : base(chauffeur, plaqueImmatriculation, marque, modele, capaciteReservoir, couleur)
+    public void ChangerNombrePassagers(int nouveauNombrePassagers)
+    {
+        if (nouveauNombrePassagers > 0 && nouveauNombrePassagers != NombrePassagers)
         {
-            this.NombrePassagers = nombrePassagers;
+            this.NombrePassagers = nouveauNombrePassagers;
+            Console.WriteLine($"Le nombre de passagers a été modifié. Nouveau nombre de passagers: {NombrePassagers}.");
         }
-
-        public void ChangerNombrePassagers(int nouveauNombrePassagers)
+        else if (nouveauNombrePassagers <= 0)
         {
-            if (nouveauNombrePassagers > 0 && nouveauNombrePassagers != this.NombrePassagers)
-            {
-                this.NombrePassagers = nouveauNombrePassagers;
-                Console.WriteLine($"Le nombre de passagers a été modifié. Nouveau nombre de passagers: {this.NombrePassagers}.");
-            }
-            else if (nouveauNombrePassagers <= 0)
-            {
-                Console.WriteLine("Le nombre de passagers doit être supérieur à zéro.");
-            }
-            else
-            {
-                Console.WriteLine("Le nouveau nombre de passagers est identique à l'ancien. Aucun changement nécessaire.");
-            }
+            Console.WriteLine("Le nombre de passagers doit être supérieur à zéro.");
+        }
+        else
+        {
+            Console.WriteLine("Le nouveau nombre de passagers est identique à l'ancien. Aucun changement nécessaire.");
         }
     }
+    public override string ToString()
+    {
+        return base.ToString() + $", Nombre de Passagers: {NombrePassagers}";
+    }
+}
 
     
 }
