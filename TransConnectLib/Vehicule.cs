db97614e8fb2b3a2c36314e50f55166224d642a2
Ga5pard <gaspard.tournadre@gmail.com>
2024-05-15 00:31:33 +0200

    Update Vehicule.cs
    Ajout de méthodes, de commentaires et simplification

--- TransConnectLib/Vehicule.cs
+++ TransConnectLib/Vehicule.cs
@@ -7,30 +7,36 @@ using System.Threading.Tasks;
 namespace TransConnectLib
 {
     public abstract class Vehicule
+{
+    // Propriétés du véhicule
+    public Chauffeur Chauffeur { get; private set; }  // Le chauffeur attribué au véhicule
+    public string PlaqueImmatriculation { get; private set; }  // La plaque d'immatriculation du véhicule
+    public string Marque { get; private set; }  // La marque du véhicule
+    public string Modele { get; private set; }  // Le modèle du véhicule
+    public double CapaciteReservoir { get; private set; }  // La capacité du réservoir du véhicule
+    public string Couleur { get; private set; }  // La couleur du véhicule
+
+    // Constructeur pour la classe Vehicule.
+    public Vehicule(Chauffeur chauffeur, string plaqueImmatriculation, string marque, string modele,
+                    double capaciteReservoir, string couleur)
     {
-        public Chauffeur chauffeur;
-        public string PlaqueImmatriculation;
-        public string Marque;
-        public string Modele;
-        public double CapaciteReservoir;
-        public string Couleur;
-
-        public Vehicule(Chauffeur chauffeur, string plaqueImmatriculation, string marque, string modele,
-                        double capaciteReservoir, string couleur)
-        {
-            this.chauffeur = chauffeur;
-            this.PlaqueImmatriculation = plaqueImmatriculation;
-            this.Marque = marque;
-            this.Modele = modele;
-            this.CapaciteReservoir = capaciteReservoir;
-            this.Couleur = couleur;
-        }
-        public void AttribuerChauffeur(Chauffeur nouveauChauffeur)
-        {
-            this.chauffeur = nouveauChauffeur;
-        }
-
-        
-
+        this.Chauffeur = chauffeur;
+        this.PlaqueImmatriculation = plaqueImmatriculation;
+        this.Marque = marque;
+        this.Modele = modele;
+        this.CapaciteReservoir = capaciteReservoir;
+        this.Couleur = couleur;
     }
+
+    // Méthodes pour la classe Vehicule.
+    public void AttribuerChauffeur(Chauffeur nouveauChauffeur)
+    {
+        this.Chauffeur = nouveauChauffeur;
+    }
+
+    public override string ToString()
+    {
+        return $"Plaque: {PlaqueImmatriculation}, Marque: {Marque}, Modèle: {Modele}, Couleur: {Couleur}, Capacité Réservoir: {CapaciteReservoir}L, Chauffeur: {Chauffeur?.Nom ?? "Aucun"}";
+    }
+}
 }
