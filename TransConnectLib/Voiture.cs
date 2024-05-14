1d7868e14c080fd85c9c5f01d2a706f52aed7185
Ga5pard <gaspard.tournadre@gmail.com>
2024-05-14 16:44:27 +0200

    Add files via upload

---
+++ TransConnectLib/Voiture.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransConnectLib
+{
+    public class Voiture : Vehicule
+    {
+        public int NombrePassagers;
+
+        public Voiture(Chauffeur chauffeur, string plaqueImmatriculation, string marque, string modele,
+                       double capaciteReservoir, string couleur, int nombrePassagers)
+                       : base(chauffeur, plaqueImmatriculation, marque, modele, capaciteReservoir, couleur)
+        {
+            this.NombrePassagers = nombrePassagers;
+        }
+
+        public void ChangerNombrePassagers(int nouveauNombrePassagers)
+        {
+            if (nouveauNombrePassagers > 0 && nouveauNombrePassagers != this.NombrePassagers)
+            {
+                this.NombrePassagers = nouveauNombrePassagers;
+                Console.WriteLine($"Le nombre de passagers a été modifié. Nouveau nombre de passagers: {this.NombrePassagers}.");
+            }
+            else if (nouveauNombrePassagers <= 0)
+            {
+                Console.WriteLine("Le nombre de passagers doit être supérieur à zéro.");
+            }
+            else
+            {
+                Console.WriteLine("Le nouveau nombre de passagers est identique à l'ancien. Aucun changement nécessaire.");
+            }
+        }
+    }
+
+    
+}
